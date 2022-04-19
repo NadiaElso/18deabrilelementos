ba05c0959e243f43eb4da01ae242c9c465a66f6c
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2022-04-18 21:39:25 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+
+ let pInformacion=document.getElementById("pinformacion");
+ let botonOcultarOmostrar=document.getElementById("ocultaromostrar");
+
+
+botonOcultarOmostrar.addEventListener("click", function () {
+  
+  pInformacion?.classList.add("color-white");
+});
+
+
+
